b04e4eedf3371c33e65e2efd1cfd5ef347d1d174
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-02-26 14:38:38 +0100

    feat: add bootstrap style to table

--- index.php
+++ index.php
@@ -10,17 +10,39 @@ include('./data.php')
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php Hotels</title>
+
+    <!-- font awersome  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- bootstrap  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
 <body>
-    <?php foreach($hotels as $hotel):?>
-        <ul>
-            <li><b><?= $hotel['name']?></b></li>
-            <li><?= $hotel['description']?></li>
-            <li><?= $hotel['parking']?></li>
-            <li><?= $hotel['vote']?></li>
-            <li><?= $hotel['distance_to_center']?></li>
-        </ul>
+
+
+<table class="table table-striped table-hover ">
+  <thead>
+    <tr>
+      <th scope="col">Nome</th>
+      <th scope="col">Descrizione</th>
+      <th scope="col">Parcheggio</th>
+      <th scope="col">Voto</th>
+      <th scope="col">Distanza dal centro</th>
+    </tr>
+  </thead>
+  <tbody>
+      <?php foreach($hotels as $hotel):?>
+    <tr>
+      <th scope="row"><?= $hotel['name']?></th>
+      <td><?= $hotel['description']?></td>
+      <td><?= $hotel['parking']?'si':'no'?></td>
+      <td><?= $hotel['vote']. ' '?><i class="fa-solid fa-star fa-sm"></i></td>
+      <td><?= $hotel['distance_to_center']?>km</td>
+    </tr>
     <?php endforeach;?>
+
+  </tbody>
+</table>
     
 </body>
 </html>
