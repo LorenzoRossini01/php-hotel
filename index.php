76f52f54ebfd4ac307a55c8ebb40cd10cdd51d1e
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-02-26 14:22:53 +0100

    feat: cycle hotel details using foreach

--- index.php
+++ index.php
@@ -12,7 +12,15 @@ include('./data.php')
     <title>php Hotels</title>
 </head>
 <body>
-
+    <?php foreach($hotels as $hotel):?>
+        <ul>
+            <li><b><?= $hotel['name']?></b></li>
+            <li><?= $hotel['description']?></li>
+            <li><?= $hotel['parking']?></li>
+            <li><?= $hotel['vote']?></li>
+            <li><?= $hotel['distance_to_center']?></li>
+        </ul>
+    <?php endforeach;?>
     
 </body>
 </html>
